43b0e7d6294d7d903e3749620bb1250c95ebcef5
Ajo <17722952+jo-0@users.noreply.github.com>
2023-11-17 14:11:30 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1 class="header">Hi Ajo</h1>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
